8cb41a71d2ba6c07fb543f179fc4c854981d0368
Dejan-Budanovic <dejanbudanovic@yahoo.com>
2020-10-15 21:36:15 +0200

    čas
    sss

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+//Active status
+linkBtn = document.getElementsByClassName("nav-link");
+
+//Active status END"
+
 //Subscribe ALERT function
 function subscribeAlert() {
   var userSubEmail = document.getElementById("InputSubEmail").value;
